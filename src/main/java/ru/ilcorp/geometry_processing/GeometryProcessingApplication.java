ec313b794823db26416294b57c199d5811575d2a
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2025-02-27 03:32:39 +0300

    inital commit

---
+++ src/main/java/ru/ilcorp/geometry_processing/GeometryProcessingApplication.java
@@ -0,0 +1,13 @@
+package ru.ilcorp.geometry_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometryProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometryProcessingApplication.class, args);
+	}
+
+}
